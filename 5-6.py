8422544e1900350b2027c5550bd9e2c6713996d4
Evgeniya Klochneva <76435382+EgeniyaKlochneva@users.noreply.github.com>
2021-01-26 10:42:16 +0300

    Update 5-6.py

--- 5-6.py
+++ 5-6.py
@@ -1,9 +1,7 @@
-s = {}
-with open('text_six.txt', 'r') as f:
+subj = {}
+with open('text_six.txt', 'r', encoding='utf-8') as f:
     for line in f:
-        #subj, lec, pract, lab = line.split()
-        print(line.split())
-        for i in line:
-            s = i[1] + i[3] + i[5]
-print(f'{s}')
+        name, stats = line.split(':')
+        num_sum = sum(map(int, "".join([i for i in stats if i == '' or ('0' <= i <= '9')])))
+    print(f'Общее количество часов по предмету - \n {subj}')
 
