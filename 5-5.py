f692cfab90c2e19d12b61528a9def1fed1552c15
Evgeniya Klochneva <76435382+EgeniyaKlochneva@users.noreply.github.com>
2021-01-26 08:14:59 +0300

    Update 5-5.py

--- 5-5.py
+++ 5-5.py
@@ -1,9 +1,5 @@
-with open('text_five.txt', 'w') as f:
+with open('text_five.txt', 'w+', encoding='utf-8') as f:
     data = f.write('5 7 89 41 55 1 33')
-with open('text_five.txt', 'r') as f:
-        s = f.readline()
-        s = list(map(int, s.split()))
-with open('text_five.txt', 'w') as f:
-        f.write(str(s))
-print(sum(s))
+    f.seek(0)
 
+    print(sum(map(int, f.readline().split())))
